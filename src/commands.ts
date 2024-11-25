9fc850a34db8d06b851c1a68a5f007833848ce50
kmate <mtkarc@gmail.com>
2024-11-25 17:32:19 +0100

    Add help

--- src/commands.ts
+++ src/commands.ts
@@ -2,9 +2,31 @@ import { RespondArguments } from "@slack/bolt";
 import * as scraping from "./scraping";
 import util from 'util';
 
+const helpRegex = /(help)|(hogy(an?)\??)|(segítség)|(segits)|(segiccse)/i;
+const lsRegex = /(ls)|(mi ?van\??)/i;
+
 export async function dispatch(command: string): Promise<RespondArguments> {
+    if (helpRegex.test(command)) {
+        return showHelp();
+    } else if (lsRegex.test(command)) {
+        return await listProducts();
+    } else {
+        return showHelp();
+    }
+}
+
+function showHelp(): Promise<RespondArguments> {
+    return Promise.resolve({
+        response_type: "ephemeral",
+        text:
+            "Ez a bot a Brew Your Mind sörfőzde kínálatát mutatja meg. Íme a használható parancsok:\n" +
+            "  - `/bym help`: megjeleníti ezt a súgót\n" +
+            "  - `/bym ls`: listázza a jelenlegi kínálatot\n"
+    });
+}
+
+async function listProducts(): Promise<RespondArguments> {
     const allProducts = await scraping.allProducts();
-    // console.log(util.inspect(allProducts, { showHidden: false, depth: null, colors: true }));
 
     return {
         response_type: "ephemeral",
